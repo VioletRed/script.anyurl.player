91fe4494c47cd0311eff0ebfa6b85aa51fc8d6e5
Violet Red <red.violet1@gmx.com>
2014-08-19 18:50:42 +0200

    Version 0.0.3 Play decoded video
    Play decoded video and nicely catch failures.

--- addon.py
+++ addon.py
@@ -2,23 +2,38 @@ import re, string, sys, os
 import urlparse
 
 import urlresolver
+import os
+from urlresolver.types import HostedMediaFile
 import xbmc
 import xbmcgui
 import xbmcaddon
 import xbmcplugin
 
+addon_id = 'script.video.anyurl'
+addon = xbmcaddon.Addon(id=addon_id)
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
-mode = args.get('mode', None)
 
-addon_id = 'script.video.anyurl'
+try:
+    mode = args['mode'][0]
+except KeyError:
+    mode = None
 
-if mode[0] is 'play_video':
-    url = "http://localhost/file.mkv"
-    file_url = urlresolver.resolve(url)
-    listitem = xbmcgui.ListItem(label = 'Title', path=file_url)
-    listitem.setInfo(type='Video')
-    # self.common.log(u"Playing video: " + repr(video['Title']) + " - " + repr(get('videoid')) + " - " + repr(video['video_url']))
-    xbmcplugin.setResolvedUrl(handle=int(sys.argv[1]), succeeded=True, listitem=listitem)
+if mode == 'play_video':
+    try:
+        url = args['url'][0]
+        media_source = HostedMediaFile(url)
+        file_url = media_source.resolve()
+        media_labels = media_source.get_media_labels()
+        li = xbmcgui.ListItem(label = media_labels['title'], path = file_url)
+        li.setThumbnailImage(media_labels['icon'])
+        xbmc.log("%s: Play video %s %s" % (addon_id, media_labels, file_url), xbmc.LOGNOTICE)
+        xbmc.Player().play(item = file_url, listitem = li)
+    except KeyError:
+        xbmc.log("%s: Missing URL" % (addon_id), xbmc.LOGNOTICE)
+    except:
+        xbmc.log("%s: Unhandled exception %s" % (addon_id, sys.exc_info()[0]), xbmc.LOGNOTICE)
+else:
+    xbmc.log("%s: Nothing to play" % (addon_id), xbmc.LOGNOTICE)
